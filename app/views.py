3d762d5e4f9d33293df00b8ef7b0b8c6888ca57f
Zim Ezeozue <zimuzostanley@gmail.com>
2015-10-23 01:42:08 +0100

    Oauth2

--- app/views.py
+++ app/views.py
@@ -5,8 +5,14 @@ import requests
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Zim'}
+    access_token = request.args.get('access_token')
+    user_id = request.args.get('user_id')
+    expires_in = request.args.get('expires_in')
+    if access_token:
+        return access_token + "\t" + user_id + "\t" + expires_in
+
     
+    user = {'nickname': 'Zim'}
     return render_template('index.html', title='Home',user=user)
 
 
@@ -18,4 +24,5 @@ def authorize():
 @app.route('/token')
 def token():
     code = request.args.get('code')
-    return code
+    r = requests.get('https://www.formstack.com/api/v2/oauth2/authorize?client_id=12999&client_secret=c3c4b56b4b&redirect_uri=http%3A%2F%2Fformmap.herokuapp.com&grant_type=authorization_code&code=' + code)
+    return "Authorized"
