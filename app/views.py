3c5d6e77cea485868febec2852a4538463053a14
Zim Ezeozue <zimuzostanley@gmail.com>
2015-10-23 08:09:25 +0100

    Oauth2

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
 from app import app
 import requests
-import json
+
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
@@ -12,14 +12,14 @@ def index():
 
 @app.route('/auth')
 def authorize():
-    r = requests.get('https://www.formstack.com/api/v2/oauth2/authorize?client_id=12999&redirect_uri=http%3A%2F%2Fformmap.herokuapp.com%2Ftoken&response_type=code')
+    r = requests.get('https://www.formstack.com/api/v2/oauth2/authorize?client_id=12999&redirect_uri=http://formmap.herokuapp.com/token&response_type=code')
     return r.text
 
 @app.route('/token')
 def token():
     code = request.args.get('code')
-    payload = {'client_id': 12999, 'client_secret': 'c3c4b56b4b', 'redirect_uri': 'http%3A%2F%2Fformmap.herokuapp.com%2Ftoken%2F', 'grant_type': 'authorization_code', 'code': str(code)}
-    r = requests.post('https://www.formstack.com/api/v2/oauth2/token', data=json.dumps(payload))
+    payload = {'client_id': 12999, 'client_secret': 'c3c4b56b4b', 'redirect_uri': 'http://formmap.herokuapp.com/token', 'grant_type': 'authorization_code', 'code': str(code)}
+    r = requests.post('https://www.formstack.com/api/v2/oauth2/token', data=payload)
     print payload
     print "Payload"
     return r.text
