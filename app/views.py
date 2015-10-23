37130f72af7667d82501087d63296476c3833e19
Zim Ezeozue <zimuzostanley@gmail.com>
2015-10-23 11:55:09 +0100

    Map

--- app/views.py
+++ app/views.py
@@ -11,7 +11,11 @@ def index():
     data = {'nickname': 'Zim', 'access_token': ''}
     if 'access_token' in session:
         data['access_token'] = session['access_token']
-        return render_template('index.html', title='Home', user=data)
+        headers = {'Authorization': 'Bearer ' + session['access_token']}
+        r = requests.get('https://www.formstack.com/api/v2/form.json', headers=headers)
+        forms = json.loads(r.text)
+        print forms
+        return render_template('index.html', data=data, forms=forms['forms'])
     else:
         return render_template('login.html')
 
@@ -34,3 +38,37 @@ def token():
 def logout():
     session.pop('access_token')
     return redirect(url_for('index'))
+
+@app.route('/fields/<form_id>')
+def fields(form_id):
+    if 'access_token' in session:
+        headers = {'Authorization': 'Bearer ' + session['access_token']}
+        r = requests.get('https://www.formstack.com/api/v2/form/' + form_id + '/field.json', headers=headers)
+        r = json.loads(r.text)
+        fields = []
+        for field in r:
+            if field['type'] == 'select' or field['type'] == 'radio':
+                fields.append(field)
+        return render_template('fields.html', fields=fields)
+    else:
+        return render_template('login.html')
+
+@app.route('/field/<field_id>')
+def field(field_id):
+    if 'access_token' in session:
+        headers = {'Authorization': 'Bearer ' + session['access_token']}
+        r = requests.get('https://www.formstack.com/api/v2/field/' + field_id + '.json', headers=headers)
+        print r.text
+        r = json.loads(r.text)
+        options = []
+        print r['options']
+        for o in r['options']:
+            pass
+            #print o
+            #options.append(r['options'][o])
+            
+        return render_template('map.html', options=options)
+    else:
+        return render_template('login.html')
+
+
