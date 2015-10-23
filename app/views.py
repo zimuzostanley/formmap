58a2f0f2bc9b5444dbfdd18ad9908dae10676a09
Zim Ezeozue <zimuzostanley@gmail.com>
2015-10-23 01:50:31 +0100

    Oauth2

--- app/views.py
+++ app/views.py
@@ -3,15 +3,8 @@ from app import app
 import requests
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    access_token = request.args.get('access_token')
-    user_id = request.args.get('user_id')
-    expires_in = request.args.get('expires_in')
-    if access_token:
-        return access_token + "\t" + user_id + "\t" + expires_in
-
-    
     user = {'nickname': 'Zim'}
     return render_template('index.html', title='Home',user=user)
 
@@ -24,5 +17,15 @@ def authorize():
 @app.route('/token')
 def token():
     code = request.args.get('code')
-    r = requests.get('https://www.formstack.com/api/v2/oauth2/authorize?client_id=12999&client_secret=c3c4b56b4b&redirect_uri=http%3A%2F%2Fformmap.herokuapp.com&grant_type=authorization_code&code=' + code)
+    r = requests.post('https://www.formstack.com/api/v2/oauth2/token', data={'client_id': '12999', 'client_secret': 'c3c4b56b4b', 'redirect_uri': 'http%3A%2F%2Fformmap.herokuapp.com%2Fauthd', 'grant_type': 'authorization_code', 'code': code})
     return "Authorized"
+
+@app.route('/authd', methods=['POST'])
+def authorized()
+    access_token = request.args.get('access_token')
+    user_id = request.args.get('user_id')
+    expires_in = request.args.get('expires_in')
+    if access_token:
+        return access_token + "\t" + user_id + "\t" + expires_in
+
+    
