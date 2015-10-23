3395f320e21fcd7a41ccefdcfb04135f5273ae6a
Zim Ezeozue <zimuzostanley@gmail.com>
2015-10-23 07:34:08 +0100

    Oauth2

--- app/views.py
+++ app/views.py
@@ -16,9 +16,7 @@ def authorize():
 
 @app.route('/token')
 def token():
-    access_token = request.args.get('access_token')
     code = request.args.get('code')
-    r = requests.post('https://www.formstack.com/api/v2/oauth2/token')
-    print access_token
+    r = requests.post('https://www.formstack.com/api/v2/oauth2/token', data={'client_id': 12999, 'client_secret': 'c3c4b56b4b', 'redirect_uri': 'http%3A%2F%2Fformmap.herokuapp.com%2Ftoken', 'grant_type': 'authorization_code', 'code': str(code)})
     return code + "\t" + r.text
     
